569c4224e6087b7271997e8b01cfa3dd0b6fdd1a
sprab <sprab@Lenovo>
2024-10-24 15:56:13 +0530

    newly commited

---
+++ core_java_programs/src/day1/subraction.java
@@ -0,0 +1,15 @@
+package day1;
+import java.util.*;
+public class subraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub``
+		int a,b,c;
+		Scanner s= new Scanner(System.in);
+		System.out.println("enter the value of a and b:");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a-b;
+		System.out.println("Subraction of "+a+" and "+b+" = "+c);
+	}
+}
